5dcb9a86e36311b0f09a279fde718145db6b8fa0
nyftheart <alexandre.kramer@ynov.com>
2023-11-08 10:55:29 +0100

    Test api

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+class Badge(BaseModel):
+    Id: int = 1
+    Nom: str = "Test"
+    More: str = "Test More"
+    Type: str = "Badge de test"
+    Nombre: int = 1
+
+@app.get("/badge", response_model=Badge)
+async def get_badge():
+    return Badge()
