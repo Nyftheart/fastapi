d5094d51e288a08f93a3038eefc052373d2f1ef5
nyftheart <alexandre.kramer@ynov.com>
2023-11-08 11:16:50 +0100

    add post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List  # Importez List depuis typing
 
 app = FastAPI()
 
@@ -8,12 +9,21 @@ async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
 
 class Badge(BaseModel):
-    Id: int = 1
-    Nom: str = "Test"
-    More: str = "Test More"
-    Type: str = "Badge de test"
-    Nombre: int = 1
+    Id: int
+    Nom: str
+    More: str
+    Type: str
+    Nombre: int
 
-@app.get("/badge", response_model=Badge)
-async def get_badge():
-    return Badge()
+# Une liste temporaire pour stocker les badges (simule une base de données)
+badges_db = []
+
+@app.get("/badge", response_model=List[Badge])  # Utilisez List[Badge]
+async def get_badges():
+    return badges_db
+
+@app.post("/badge", response_model=Badge)
+async def create_badge(badge: Badge):
+    # Ajouter le badge à la liste des badges
+    badges_db.append(badge)
+    return badge
